746facbcd1e62d06389fc8a7300613eef9381fc3
Aer054 <112958865+Aer054@users.noreply.github.com>
2023-01-20 22:20:38 +0700

    Добавьте файлы проекта.

---
+++ Model.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab4
+{
+    internal class Model
+    {
+        private int age;
+        private double height;
+        private double weight;
+        private string bodyType;
+        private double distance;
+        private double calories;
+        #region gettrs
+        public int Age
+        {
+            get
+            {
+                return this.age;
+            }
+
+            set
+            {
+                this.age = value;
+            }
+        }
+        public double Height
+        {
+            get
+            {
+                return this.height;
+            }
+
+            set
+            {
+                this.height = value;
+            }
+        }
+        public double Weight
+        {
+            get
+            {
+                return this.weight;
+            }
+
+            set
+            {
+                this.weight = value;
+            }
+        }
+        public string BodyType
+        {
+            get
+            {
+                return this.bodyType;
+            }
+
+            set
+            {
+                this.bodyType = value;
+            }
+        }
+        public double Distance
+        {
+            get
+            {
+                return this.distance;
+            }
+
+            set
+            {
+                this.distance = value;
+            }
+        }
+        public double Calories
+        {
+            get
+            {
+                return this.calories;
+            }
+
+            set
+            {
+                this.calories = value;
+            }
+        }
+        #endregion
+
+    }
+}
---
+++ ProfileViewModel.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+
+namespace Lab4
+{
+     class ProfileViewModel : INotifyPropertyChanged
+    {
+        public ICommand Enter { set; get; }
+        private async void Command_Execute_Enter(object parameter)
+        {
+           Window1 Window=new Window1();
+           Window.Show();
+           Application.Current.MainWindow.Close();
+        }
+        public ProfileViewModel()
+        {
+            Enter = new RelayCommand(Command_Execute_Enter);
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void OnPropertyChanged([CallerMemberName] string prop = "")
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(prop));
+        }
+    }
+
+}
